45b3c5deae173c4a14e1bb549eede2ffdaf2883c
emil2709 <emil.nedregard@gmail.com>
2019-05-07 15:09:00 +0200

    change and rename

---
+++ tester.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+arr=(
+	8kb
+	80kb
+	800kb
+	8mb
+	80mb	
+)
+
+for x in {1..25}
+do
+	for i in "${arr[@]}";
+	do
+		/usr/bin/time -p sh -c 'taskset -c 0 '$i'/a.out & taskset -c 0 '$i'/b.out'  |& awk 'FNR == 1{print $2}' >> memtester.txt
+		echo "$i" >> memtester.txt
+		echo "$1" >> memtester.txt
+		sleep 2
+	done
+done
